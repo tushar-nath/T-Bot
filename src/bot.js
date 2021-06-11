a9ec1e2799389ba8c92195ed8c7001de69fd3176
tusharnath10 <tusharnath10@gmail.com>
2021-06-12 00:48:30 +0530

    new changes

--- src/bot.js
+++ src/bot.js
@@ -1,115 +1,54 @@
-require('dotenv').config();
+const Discord = require('discord.js');
+const config = require('./config.json');
+const client = new Discord.Client();
+const bot = new Discord.Client();
 
-const  { Client, WebhookClient } = require('discord.js');
-const client = new Client({
-  partials: ['MESSAGE', 'REACTION']
+const { prefix, token } = require('./config.json');
+
+client.once('ready', () => {
+	console.log('Ready!');
 });
 
-const webhookClient = new WebhookClient(
-  process.env.WEBHOOK_ID,
-  process.env.WEBHOOK_TOKEN,
-);
-
-const PREFIX = "$";
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+	
+	if (command === 'help') {
+		message.channel.send('Supported commands :\n$help - Displays the help menu\n$ping - Should respond with a pong and display the latency\n$avatar - Displays the avatar of the user\n$server - Displays the name of the server\n$user-info - Displays the information of the user and its ID');
+	} else if (command === 'ping') {
+		const timeTaken = Date.now() - message.createdTimestamp;
+    	message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+	} else if (command === 'beep') {
+		message.channel.send('Boop.');
+	} else if (command === `server`) {
+		message.channel.send(`This server's name is: ${message.guild.name}`);
+	} else if (command === `user-info`) {
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	} else if (command === 'avatar') {
+		if (!message.mentions.users.size) {
+			return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: 'png', dynamic: true })}>`);
+		}
+		
+		const avatarList = message.mentions.users.map(user => {
+			return `${user.username}'s avatar: <${user.displayAvatarURL({ format: 'png', dynamic: true })}>`;
+		});
+	
+		message.channel.send(avatarList);
+	}
+});
 
 client.on('ready', () => {
-    console.log(`${client.user.tag} has logged in.`);
+    console.log(`Logged in as ${client.user.tag}!`);
+
+    client.user.setPresence({
+        status: 'online',
+        activity: {
+            name: "$help",
+            type: "PLAYING"
+        }
+    });
 });
 
-client.on('message', async (message) => {
-  if (message.author.bot) return;
-  if (message.content === `$ping`) {
-		message.channel.send('Pong.');
-	} else if (message.content === `$beep`) {
-		message.channel.send('Boop.');
-	} else if (message.content === `$server`) {
-		message.channel.send(`This server's name is: ${message.guild.name}`);
-	}
-  if (message.content === 'hello') {
-		message.channel.send('hi, there!');
-  }
-  if (message.content === 'how are you?') {
-		message.channel.send('i am good, thanks!');
-	}
-  if (message.content.startsWith(PREFIX)) {
-    const [CMD_NAME, ...args] = message.content
-      .trim()
-      .substring(PREFIX.length)
-      .split(/\s+/);
-    if (CMD_NAME === 'kick') {
-      if (!message.member.hasPermission('KICK_MEMBERS'))
-        return message.reply('You do not have permissions to use that command');
-      if (args.length === 0)
-        return message.reply('Please provide an ID');
-      const member = message.guild.members.cache.get(args[0]);
-      if (member) {
-        member
-          .kick()
-          .then((member) => message.channel.send(`${member} was kicked.`))
-          .catch((err) => message.channel.send('I cannot kick that user :('));
-      } else {
-        message.channel.send('That member was not found');
-      }
-    } else if (CMD_NAME === 'ban') {
-      if (!message.member.hasPermission('BAN_MEMBERS'))
-        return message.reply("You do not have permissions to use that command");
-      if (args.length === 0) return message.reply("Please provide an ID");
-      try {
-        const user = await message.guild.members.ban(args[0]);
-        message.channel.send('User was banned successfully');
-      } catch (err) {
-        console.log(err);
-        message.channel.send('An error occured. Either I do not have permissions or the user was not found');
-      }
-    } else if (CMD_NAME === 'announce') {
-      console.log(args);
-      const msg = args.join(' ');
-      console.log(msg);
-      webhookClient.send(msg);
-    }
-  }
-});
 
-client.on('messageReactionAdd', (reaction, user) => {
-  const { name } = reaction.emoji;
-  const member = reaction.message.guild.members.cache.get(user.id);
-  if (reaction.message.id === '738666523408990258') {
-    switch (name) {
-      case '🍎':
-        member.roles.add('738664659103776818');
-        break;
-      case '🍌':
-        member.roles.add('738664632838782998');
-        break;
-      case '🍇':
-        member.roles.add('738664618511171634');
-        break;
-      case '🍑':
-        member.roles.add('738664590178779167');
-        break;
-    }
-  }
-});
-
-client.on('messageReactionRemove', (reaction, user) => {
-  const { name } = reaction.emoji;
-  const member = reaction.message.guild.members.cache.get(user.id);
-  if (reaction.message.id === '738666523408990258') {
-    switch (name) {
-      case '🍎':
-        member.roles.remove('738664659103776818');
-        break;
-      case '🍌':
-        member.roles.remove('738664632838782998');
-        break;
-      case '🍇':
-        member.roles.remove('738664618511171634');
-        break;
-      case '🍑':
-        member.roles.remove('738664590178779167');
-        break;
-    }
-  }
-});
-
-client.login(process.env.DISCORDJS_BOT_TOKEN);
+client.login('ODUxNTM3MDQxNDI1OTU2ODk0.YL5tqw.jMQZfV5-PQT8_q0wP1Bh2rSSq-8');
